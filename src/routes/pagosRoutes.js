6c49f4325cc1d3289c3d0cf844efe55ae1a18961
JavierYepezRamirez <157386401+JavierYepezRamirez@users.noreply.github.com>
2025-02-25 13:53:32 -0600

    Delete src directory

--- src/routes/pagosRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getPagos } = require("../controllers/pagosController");
-const authMiddleware = require("../middlewares/authMiddleware");
-const router = express.Router();
-
-router.get("/",authMiddleware, getPagos);
-
-module.exports = router;
--- src/utils/generateToken.js
+++
@@ -1,7 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const generateToken = (uid) => {
-  return jwt.sign({ uid }, process.env.JWT_SECRET, { expiresIn: "1h" });
-};
-
-module.exports = generateToken;
